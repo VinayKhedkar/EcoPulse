5773971e3e4d5d40731e91dcc7e9a598cc0b86fe
Ankit Bhagat <annonymous007banda@gmail.com>
2025-03-27 19:35:26 +0530

    feat: configure next.config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import nextPWA from '@ducanh2912/next-pwa'
+
+const withPWA = nextPWA({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+	cacheOnFrontEndNav: true,
+	aggressiveFrontEndNavChaching: true,
+	reloadOnOnline: true,
+	disable: false, //process.env.NODE_ENV === 'development',
+	workboxOptions: {
+		disableDevLogs: true,
+	},
+})
+
+const nextConfig = withPWA({
+	reactStrictMode: true,
+})
 
 export default nextConfig
