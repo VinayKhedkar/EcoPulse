49de071492424f129f0d7526e3ac6fd293d80afa
Ankit Bhagat <annonymous007banda@gmail.com>
2025-04-05 22:39:35 +0530

    fix: app.py

--- server/app.py
+++ server/app.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     if os.getenv("FLASK_ENV") == "development":
         app.run(debug=True)
     else:
-        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+        app.run()
